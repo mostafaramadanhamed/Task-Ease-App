41e8cadd79df3474c64f5fc9c8f872e81719311a
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2024-06-19 21:01:51 +0300

    add routes

---
+++ lib/core/routing/routes.dart
@@ -0,0 +1,9 @@
+class Routes {
+  static const String initialScreen = '/initialScreen';
+  static const String onBoardingScreen = '/onBoardingScreen';
+  static const String homeScreen = '/homeScreen';
+  static const String addTaskScreen = '/homeScreen';
+  static const String editTaskScreen = '/homeScreen';
+
+
+}
