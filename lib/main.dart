78d64554ac016e583bdd1b65a2a31353cdc392a6
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2024-06-19 12:34:47 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:task_ease/task_ease_app.dart';
+
+void main() {
+  runApp(const TaskEaseApp());
+}
---
+++ lib/task_ease_app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TaskEaseApp extends StatelessWidget {
+  const TaskEaseApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    );
+  }
+}
