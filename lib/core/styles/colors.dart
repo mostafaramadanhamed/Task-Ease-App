25e04b9819059ec61b5d5dead24fc1d4eb2d5c77
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2024-06-20 10:30:07 +0300

    add colors

---
+++ lib/core/styles/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+abstract class ColorsManager{
+  static const kMainColor=Color(0xff24252C);
+  static const kSecondaryColor=Color(0xff6E6A7C);
+  static const kPrimaryColor=Color(0xff5F33E1);
+}
