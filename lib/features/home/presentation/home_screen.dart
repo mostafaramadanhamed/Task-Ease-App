fa5aa4bf0694c44fc975c7263c5b9a5db46f68c8
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2024-06-23 12:06:12 +0300

    create floating action button

--- lib/features/home/presentation/home_screen.dart
+++ lib/features/home/presentation/home_screen.dart
@@ -3,14 +3,19 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:task_ease/core/styles/colors.dart';
 import 'package:task_ease/core/utils/spacing_extension.dart';
 import 'package:task_ease/features/home/presentation/widget/hello_row.dart';
-
-import '../../../core/styles/text_styles.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        backgroundColor: ColorsManager.kPrimaryColor.withOpacity(0.3),
+        elevation: 0,
+        child: const Icon(Icons.add_outlined,color: ColorsManager.kPrimaryColor,),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       body: SafeArea(
         child: Padding(
           padding:  EdgeInsets.symmetric(horizontal: 16.0.w),
