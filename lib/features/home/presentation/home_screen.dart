9281dac37a39eaa895f756eb9bd803f2e5dd1fcd
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2024-06-24 11:20:05 +0300

    add date row

--- lib/features/home/presentation/home_screen.dart
+++ lib/features/home/presentation/home_screen.dart
@@ -1,8 +1,10 @@
+import 'package:easy_date_timeline/easy_date_timeline.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:task_ease/core/routing/routes.dart';
+import 'package:task_ease/core/routing/routing_extension.dart';
 import 'package:task_ease/core/styles/colors.dart';
 import 'package:task_ease/core/utils/spacing_extension.dart';
-import 'package:task_ease/features/home/presentation/widget/hello_row.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
@@ -10,7 +12,9 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: (){},
+        onPressed: (){
+          context.pushNamed(Routes.addTaskScreen);
+        },
         backgroundColor: ColorsManager.kPrimaryColor.withOpacity(0.3),
         elevation: 0,
         child: const Icon(Icons.add_outlined,color: ColorsManager.kPrimaryColor,),
@@ -22,7 +26,50 @@ class HomeScreen extends StatelessWidget {
           child: Column(
             children: [
               16.ph,
-              const HelloRow(),
+              EasyDateTimeLine(
+                initialDate: DateTime.now(),
+                onDateChange: (selectedDate) {
+                  //`selectedDate` the new date selected.
+                },
+                headerProps: const EasyHeaderProps(
+                  monthPickerType: MonthPickerType.switcher,
+                  dateFormatter: DateFormatter.fullDateDMY(),
+                ),
+                dayProps:  EasyDayProps(
+                  dayStructure: DayStructure.dayStrDayNum,
+                  inactiveDayStyle: DayStyle(
+                    decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.all(Radius.circular(8)),
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                          const Color(0xff8426D6).withOpacity(0.5),
+                          const Color(0xff8426D6).withOpacity(0.3),
+                          const Color(0xff8426D6).withOpacity(0.2),
+                          const Color(0xff8426D6).withOpacity(0.1),
+                          const Color(0xff8426D6).withOpacity(0.2),
+                          const Color(0xff8426D6).withOpacity(0.3),
+                          const Color(0xff8426D6).withOpacity(0.5),
+                          ],
+                      ),
+                    ),
+                  ),
+                  activeDayStyle: const DayStyle(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(8)),
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                          Color(0xff3371FF),
+                          Color(0xff8426D6),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
