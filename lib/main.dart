8c43c9400717dee66af0c52ef6ea492e127f9d38
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2024-06-30 10:17:59 +0300

    create task box

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:task_ease/core/routing/app_router.dart';
+import 'package:task_ease/core/utils/shared_strings.dart';
 import 'package:task_ease/task_ease_app.dart';
 
 import 'features/add task/data/models/task_model.dart';
@@ -8,5 +9,6 @@ import 'features/add task/data/models/task_model.dart';
 void main() async{
   await Hive.initFlutter();
   Hive.registerAdapter(TaskModelAdapter());
+  await Hive.openBox<TaskModel>(SharedStrings.kTaskBox);
   runApp( TaskEaseApp(appRouter: AppRouter(),));
 }
