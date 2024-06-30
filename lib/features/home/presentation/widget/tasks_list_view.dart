834eb3f950256c1da980f716678b5bb3807c85ea
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2024-06-30 13:04:44 +0300

    create task list view

---
+++ lib/features/home/presentation/widget/tasks_list_view.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:task_ease/features/add%20task/data/models/task_model.dart';
+import 'package:task_ease/features/home/logic/fetch%20task%20cubit/fetch_task_cubit.dart';
+import 'package:task_ease/features/home/presentation/widget/task_item.dart';
+
+class TaskListview extends StatelessWidget {
+  const TaskListview({Key? key,}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<FetchTaskCubit, FetchTaskState>(
+      builder: (context, state) {
+        List<TaskModel>tasks = BlocProvider
+            .of<FetchTaskCubit>(context)
+            .tasks!;
+        return ListView.builder(
+          physics: const BouncingScrollPhysics(),
+          itemCount: tasks.length,
+          padding: EdgeInsets.zero,
+          itemBuilder: (context, index) {
+            return TaskItem(
+              taskModel: tasks[index],
+            );
+          },
+        );
+      },
+    );
+  }
+}
