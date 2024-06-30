31e056ef18cd865a2e81455aa167383ad5ef9987
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2024-06-30 13:10:04 +0300

    edit task model for edit and delete

--- lib/features/add task/data/models/task_model.dart
+++ lib/features/add task/data/models/task_model.dart
@@ -1,9 +1,8 @@
-import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 part 'task_model.g.dart';
 
 @HiveType(typeId: 1)
-class TaskModel {
+class TaskModel extends HiveObject{
   @HiveField(0)
   final int id;
   @HiveField(1)
