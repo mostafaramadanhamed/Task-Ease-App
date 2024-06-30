98559d9c7a23e9f8ca690fe78cd66b43fb1dedf0
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2024-06-30 11:11:33 +0300

    refactor add task screen

--- lib/features/add task/presentation/add_task_screen.dart
+++ lib/features/add task/presentation/add_task_screen.dart
@@ -1,57 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:intl/intl.dart';
-import 'package:task_ease/core/routing/routing_extension.dart';
-import 'package:task_ease/core/utils/spacing_extension.dart';
-import 'package:task_ease/core/widgets/app_button.dart';
-import 'package:task_ease/core/widgets/app_text_field.dart';
 import 'package:task_ease/core/widgets/default_appbar.dart';
-import 'package:task_ease/features/add%20task/data/models/task_model.dart';
 import 'package:task_ease/features/add%20task/logic/add%20task%20cubit/add_task_cubit.dart';
-import '../../../core/styles/colors.dart';
-import '../../../core/styles/text_styles.dart';
+import 'package:task_ease/features/add%20task/presentation/widget/task_form.dart';
 
-class AddTaskScreen extends StatefulWidget {
+class AddTaskScreen extends StatelessWidget {
 
    const AddTaskScreen({super.key});
-
-  @override
-  State<AddTaskScreen> createState() => _AddTaskScreenState();
-}
-
-class _AddTaskScreenState extends State<AddTaskScreen> {
-  AutovalidateMode autoValidateMode=AutovalidateMode.disabled;
-  final GlobalKey<FormState> formKey =GlobalKey();
-  TextEditingController taskGroupController=TextEditingController();
-  TextEditingController projectNameController=TextEditingController();
-  TextEditingController descriptionController=TextEditingController();
-  DateTime ?initialDate = DateTime.now(); // Default to current date
-   TimeOfDay ?initialTime = TimeOfDay.now(); // Default to current date
-
-  Future<DateTime?> selectDate(BuildContext context, Function(DateTime) onDateSelected) async {
-    final DateTime? picked = await showDatePicker(
-      context: context,
-      initialDate: DateTime.now(),
-      firstDate: DateTime(2000),
-      lastDate: DateTime(2100),
-    );
-    if (picked != null) {
-      onDateSelected(picked);  // Call the callback with selected date
-    }
-    return picked;
-  }
-Future<TimeOfDay?> selectTime(BuildContext context, Function(TimeOfDay) onTimeSelected) async {
-    final TimeOfDay? picked = await showTimePicker(
-      context: context, initialTime: TimeOfDay.now(),
-
-    );
-    if (picked != null) {
-      onTimeSelected(picked);  // Call the callback with selected date
-    }
-    return picked;
-  }
-
 
   @override
   Widget build(BuildContext context) {
@@ -61,77 +17,8 @@ Future<TimeOfDay?> selectTime(BuildContext context, Function(TimeOfDay) onTimeSe
       appBar: buildDefaultAppBar("Add Project", context),
       body:  Padding(
         padding:EdgeInsets.symmetric(horizontal: 22.0.w),
-        child: SingleChildScrollView(
-          child: Form(
-            key: formKey,
-            child: Column(
-              children: [
-                24.ph,
-                 AppTextFormField(hintText: "Task Group",controller: taskGroupController,),
-                24.ph,
-                 AppTextFormField(hintText: "Project Name", controller: projectNameController,),
-                24.ph,
-                 AppTextFormField(hintText: "Description",maxLine: 6,textInputType: TextInputType.multiline, controller: descriptionController,),
-                24.ph,
-                ListTile(
-                  onTap: () async {
-                   initialDate = await selectDate(context, (date) {
-                      setState(() {
-                        initialDate = date;
-                      });
-                    });
-                  },
-                  tileColor: Colors.white,
-                  splashColor: ColorsManager.kPrimaryLightColor,
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.r)),
-                  leading: const Icon(Icons.calendar_month_sharp, color: ColorsManager.kPrimaryColor),
-                  title: Text(
-                    DateFormat.yMMMEd().format(initialDate ?? DateTime.now()),
-                    style: TextStyles.font18SemiBold,
-                  ),
-                ),
-                24.ph,
-                ListTile(
-                  onTap: () async {
-                   initialTime = await selectTime(context, (time) {
-                      setState(() {
-                        initialTime = time;
-                      });
-                    });
-                  },
-                  tileColor: Colors.white,
-                  splashColor: ColorsManager.kPrimaryLightColor,
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.r)),
-                  leading: const Icon(Icons.watch_later, color: ColorsManager.kPrimaryColor),
-                  title: Text(
-                  initialTime!.format(context),
-                    style: TextStyles.font18SemiBold,
-                  ),
-                ),
-                24.ph,
-                BlocBuilder<AddTaskCubit, AddTaskState>(
-  builder: (context, state) {
-    return AppTextButton(buttonText: "Add Project", onPressed:() {
-      if(formKey.currentState!.validate()){
-        formKey.currentState!.save();
-
-        var taskModel=TaskModel(id:  hashCode,
-            taskGroup: taskGroupController.text,
-            projectName: projectNameController.text,
-            description: descriptionController.text, selectedDate: initialDate!, selectedTime: initialTime!.format(context));
-        BlocProvider.of<AddTaskCubit>(context).addTask(taskModel);
-        context.pop();
-      }
-      else{
-        autoValidateMode=AutovalidateMode.always;
-        setState((){});
-      }
-                });
-  },
-)
-              ],
-            ),
-          ),
+        child: const SingleChildScrollView(
+          child: TaskForm(),
         ),
       ),
     ),
