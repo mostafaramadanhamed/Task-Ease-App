49c65997ac9dfa5fb2f695539eb3fa77471c15e5
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2024-06-30 10:30:09 +0300

    edit add task state

--- lib/features/add task/logic/add task cubit/add_task_state.dart
+++ lib/features/add task/logic/add task cubit/add_task_state.dart
@@ -3,6 +3,7 @@ part of 'add_task_cubit.dart';
 abstract class AddTaskState {}
 
 final class AddTaskInitial extends AddTaskState {}
+final class AddTaskLoading extends AddTaskState {}
 final class AddTaskSuccess extends AddTaskState {}
 final class AddTaskFailure extends AddTaskState {
   final String error;
