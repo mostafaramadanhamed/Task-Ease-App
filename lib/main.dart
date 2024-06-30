1de25708bd02e24d78a9f6b1f5917aabe98d2af0
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2024-06-30 10:12:51 +0300

    init hive in root app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:task_ease/core/routing/app_router.dart';
 import 'package:task_ease/task_ease_app.dart';
 
-void main() {
+import 'features/add task/data/models/task_model.dart';
+
+void main() async{
+  await Hive.initFlutter();
+  Hive.registerAdapter(TaskModelAdapter());
   runApp( TaskEaseApp(appRouter: AppRouter(),));
 }
